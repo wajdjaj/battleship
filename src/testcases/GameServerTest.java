9e6aeb5aa8b03ee39df12e7d9e74c382c3d2634a
Henrik Josefsson <josefssonhenrik@hotmail.com>
2015-04-14 13:35:29 +0200

    separate folder for tests

---
+++ src/testcases/GameServerTest.java
@@ -0,0 +1,27 @@
+package testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import server.GameServer;
+import client.GameClient;
+
+public class GameServerTest {
+	public static void oneTimeSetUp(){
+		GameServer server;
+		GameClient client1;
+		GameClient client2;
+	}
+	@Test
+	public void testWaitPlayers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitClientBoardSetup() {
+
+		fail("Not yet implemented");
+	}
+
+}
