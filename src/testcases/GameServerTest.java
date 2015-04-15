3efebf67bc8d92fc14e2fe437dbee66c963411f1
Henrik Josefsson <josefssonhenrik@hotmail.com>
2015-04-15 02:29:35 +0200

    Fixed BoardSetup test
    The BoardSetup test is now functional

--- src/testcases/GameServerTest.java
+++ src/testcases/GameServerTest.java
@@ -2,7 +2,11 @@ package testcases;
 
 import static org.junit.Assert.*;
 
-import java.io.StringReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 import org.junit.Test;
 
@@ -11,31 +15,51 @@ import client.GameClient;
 
 public class GameServerTest {
 	@Test
-	public void testWaitPlayers() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testWaitClientBoardSetup() {
 		GameServer server = new GameServer();
 		new Thread(server).start();
-		new Thread(new GameClient(new StringReader("1 A1 A1"))).start();
-		new Thread(new GameClient(new StringReader("1 A1 A1"))).start();
-		
+		FileReader fromFile0 = null;
+		FileReader fromFile1 = null;
+		try{
+			fromFile0 = new FileReader("datafiles/boardsetup");
+			fromFile1 = new FileReader("datafiles/boardsetup");
+		}catch(Exception e){
+			System.out.println("Couldn't open file");
+			System.exit(1);
+		}
+			
+		new Thread(new GameClient(fromFile0)).start();
+		new Thread(new GameClient(fromFile1)).start();
+		try {
+			Thread.sleep(5000);			
+		} catch (InterruptedException e) {			
+			e.printStackTrace();
+			System.exit(1);
+		}		
+		try{
+			fromFile0.close();
+			fromFile1.close();			
+		}catch(IOException e){
+			System.exit(1);
+		}		
 		int board0[][] = server.getBoard(0);
 		int board1[][] = server.getBoard(1);
+		Scanner scanner = null;;
+		try {
+			scanner = new Scanner(new File("datafiles/truth"));
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldn't open file");
+			System.exit(1);
+		}
 		for (int i = 0; i < 8; i++){
-			for (int j = 0; i < 8; j++){
-				if (i == 0 && j == 0){
-					assertTrue(board0[i][j] == 1);
-					assertTrue(board1[i][j] == 1);
-				}
-				else{
-					assertTrue(board0[i][j] == 0);
-					assertTrue(board1[i][j] == 0);
-				}
+			for (int j = 0; j < 8; j++){
+				int val = scanner.nextInt();
+				assertTrue(board0[j][i] == val);
+				assertTrue(board1[j][i] == val);
 			}
-		}				
+			System.out.println();
+		}
+		scanner.close();
 	}
 
 }
