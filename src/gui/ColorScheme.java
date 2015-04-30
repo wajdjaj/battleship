8865ed6facc9a6d96694ee3ef76a1a3f9fa07203
Henrik Josefsson <josefssonhenrik@hotmail.com>
2015-04-30 22:20:11 +0200

    Global coloring scheme
    Colormapping in one centralized
    file instead of all over the
    code. Most of the code has been
    updated accordingly.

---
+++ src/gui/ColorScheme.java
@@ -0,0 +1,13 @@
+package gui;
+
+import java.awt.Color;
+
+//global colorscheme
+public class ColorScheme {
+	static Color board = Color.white;
+	static Color miss = Color.black;
+	static Color hitGood = Color.green;
+	static Color hitBad = Color.red;
+	static Color ship = Color.cyan;
+	
+}
